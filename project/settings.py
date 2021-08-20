4e79d3b0bf1bae4cc4df4d9da2ae59e2d8906951
root <ayemlojoseph@gmail.com>
2021-08-20 12:14:50 +0100

    changes made to settings.py on whiteNose

--- project/settings.py
+++ project/settings.py
@@ -127,6 +127,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+# whiteNose for staticfiles
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 #django Coresheader
