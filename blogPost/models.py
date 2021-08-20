b0a3d4b7c8b7baebdfae934d36a0570ceafcf265
root <ayemlojoseph@gmail.com>
2021-08-20 20:38:48 +0100

    changes made to models

--- blogPost/models.py
+++ blogPost/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
 
     author = models.CharField(max_length=25, null=True, blank=True)
     title = models.CharField(max_length=100, null=True, blank=True)
-    description = models.CharField(max_length=255, null=True, blank=True)
+    description = TextField.CharField(max_length=1000, null=True, blank=True)
     content = models.TextField(max_length=1000, null=True, blank=True)
     #install Pillow for DRF so as to use image field
     img = models.ImageField(null=True, upload_to=imageUploadPath, blank=True) 
