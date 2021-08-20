df874e5e6e251c6621ba50c0b6d4f2ca6d62b25f
root <ayemlojoseph@gmail.com>
2021-08-20 19:51:31 +0100

    url modified

--- project/urls.py
+++ project/urls.py
@@ -23,7 +23,7 @@ schema_view = get_schema_view(
 
 #urls
 urlpatterns = [
-    path ('api/', include('blogPost.urls')),
+    path ('', include('blogPost.urls')),
     path('admin/', admin.site.urls),
     path('', schema_view.with_ui('swagger',
                                  cache_timeout=0), name='schema-swagger-ui'),
